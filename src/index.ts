51e39da7007111673a38503e5b1d0f15e9483f4b
Fozil Zayn <fozil.z@appliedlabs.io>
2023-08-14 18:05:36 +0500

    deploy

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv'
+dotenv.config();
+import config from './config'
+
+import app from './server'
+
+const PORT = config.port
+
+app.listen(PORT, () => {
+	console.log("Listening on port: ", PORT);
+});
